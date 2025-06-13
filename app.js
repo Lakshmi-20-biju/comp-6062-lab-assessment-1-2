1d07d150bf5b9fad49a974fd00b9b67fa611257e
Lakshmi-20-biju <lakshmibnair20@gmail.com>
2025-06-12 20:58:44 -0400

    changes

--- app.js
+++
@@ -1,8 +0,0 @@
-
-const Studentname="Lakshmi Biju Nair";
-const Studentnumber="1229562";
-const result = 'Studentname'+'-'+'Studentnumber';
-console.log(result);
-const headingPrimaryContent  = document.querySelector('h1');
-headingPrimaryContent.classList.add("headingPrimaryContent");
-headingPrimaryContent.innerHTML=result;
